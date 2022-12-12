86065594891dd09020ced20752fd4738ca6c04ce
praduman03 <113344907+praduman03@users.noreply.github.com>
2022-12-12 15:56:27 +0530

    2 files modified

--- promise-async.js
+++ promise-async.js
@@ -2,20 +2,44 @@ const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
 const newCookie = {name:"Biscotti Cookies"};
 
 // Progression 1: create a function to get all the cookies
-
-   // Progression 2: using setTimeout() - use 1000 units for time parameter
-
-
+function getCookies(){
+   // Progression 2: using setTimeout() -- use 1000 units for time parameter
+     setTimeout(()=>{
+       let output = ""; 
+       for(i=0;i<cookies.length;i++){
+         output+=cookies[i].name + " "
+       }
+       document.body.innerHTML = output;
+     },1000);
+   };
 //Progression 3: Create a function to creat cookies and create an object of Promise.
-
-
-    // Progression 4: adding two parameters and using setTimeout() - use 2000 units for time parameter
-
-      
-      // Progression 5: handling errors and adding new cookie to the list
-
-
+function createCookie(newCookie){
+   return new Promise((resolve,reject)=>{
+ 
+     // Progression 4: adding two parameters and using setTimeout()
+     setTimeout(()=>{
+       
+       // Progression 5: handling errors and adding new cookie to the list
+       cookies.push(newCookie);
+ 
+       const error = false;
+ 
+       if(!error){
+         resolve();
+       }
+       else{
+         reject('There are no such cookies')
+       }
+ 
+     },2000);
+   }
+     
+   );
+ };
 // Progression 7: creating a new async function
-
+async function init(){
+   await createCookie(newCookie);
+   getCookies();
+   }
 // calling the new async function
-
+init();
